ae332b877f292de9dfcf9ad1ebcb317b9585f108
paulkabzz <107804154+paulkabzz@users.noreply.github.com>
2024-05-05 01:46:27 +0200

    Update script.js

--- www.paulfreestyle.co.za/js/script.js
+++ www.paulfreestyle.co.za/js/script.js
@@ -1,225 +1,317 @@
-const wrapper = document.querySelector('.wrapper'),
-    musicImg = document.querySelectorAll('.album-img'),
-    musicName = document.querySelectorAll('.name'),
-    artist = document.querySelectorAll('.artist'),
-    playPauseButton = document.querySelectorAll('.play-pause'),
-    prev = document.querySelector('#prev'),
-    next = document.querySelector('#next'),
-    mainAudio = document.querySelector('#main-audio'),
-    progressArea = document.querySelector('.progress-area'),
-    progressBar = document.querySelector('.progress-bar'),
-    searchresult = document.querySelectorAll('.searchresult'),
-    artistImg = document.querySelectorAll('.artisimg'),
-    lyrics = document.querySelector('.lyric-text'),
-    smallPlayButton = document.querySelector('.small-play'),
-    header = document.querySelector('.header'),
-    axc = document.querySelector('.axc'),
-    headerOverlay = document.querySelector('.header-overlay'),
-    headerBtn = document.querySelector('.header-btn');
-    let ft = document.querySelector('.feature')
+class MusicPlayer {
+    constructor() {
+        this.wrapper = document.querySelector('.wrapper');
+        this.musicImg = document.querySelectorAll('.album-img');
+        this.musicName = document.querySelectorAll('.name');
+        this.artist = document.querySelectorAll('.artist');
+        this.playPauseButton = document.querySelectorAll('.play-pause');
+        this.prev = document.querySelector('#prev');
+        this.next = document.querySelector('#next');
+        this.mainAudio = document.querySelector('#main-audio');
+        this.progressArea = document.querySelector('.progress-area');
+        this.progressBar = document.querySelector('.progress-bar');
+        this.lyrics = document.querySelector('.lyric-text');
+        this.smallPlayButton = document.querySelector('.small-play');
+        this.header = document.querySelector('.header');
+        this.headerOverlay = document.querySelector('.header-overlay');
+        this.headerBtn = document.querySelector('.header-btn');
+        this.feature = document.querySelector('.feature');
 
+        this.songsDir = '../assets/songs/';
+        this.artistsDir = '../assets/images/artists/';
+        this.albumsDir = '../assets/images/album/';
+        this.musicIndex = 0;
+        this.isPaused = true;
 
-const SONGS_DIR = '../assets/songs/',
-      ARTISTS_DIR = '../assets/images/artists/',
-      ALBUMS_DIR = '../assets/images/album/';
-
-let musicIndex,
-    songName,
-    isPaused = true;
-    
-window.onload = () => {
-    // get the index of the song based on the location.hash
-    let songName = window.location.hash.substr(1);
-  
-    if (!songName) {
-      musicIndex = 0;
-      songName = musicArray[0].name.toLowerCase();
-      window.location.hash = songName;
-    } else {
-      musicIndex = musicArray.findIndex(song => song.name.toLowerCase() === songName.replace(/%20/g, ' '));
-      window.location.hash = musicArray[musicIndex].name.toLowerCase();
-    }
-  
-    // load the last played song from local storage if available
-    const lastPlayedSongName = localStorage.getItem('lastPlayedSongName');
-    if (lastPlayedSongName) {
-      const lastPlayedSongIndex = musicArray.findIndex(song => song.name.toLowerCase() === lastPlayedSongName.toLowerCase());
-      if (lastPlayedSongIndex !== -1) {
-        musicIndex = lastPlayedSongIndex;
-        songName = musicArray[lastPlayedSongIndex].name.toLowerCase();
-      }
-    }
-      
-        loadMusic(musicIndex);
-};
-      
-//Load the song at the ith index
-const loadMusic = i => {
-    if (!musicArray[i]) {
-      console.error(`There's no song at index: ${i}`)
+        this.bindEvents();
+        this.loadMusic(this.musicIndex);
+        this.loadSettings();
     }
 
-    document.title = musicArray[i].name + ' - ' + musicArray[i].artist;
-    musicName.forEach(name => name.textContent = musicArray[i].name);
-    artist.forEach(artist => artist.textContent = musicArray[i].artist);
-    artistImg.forEach(img => img.src = `${ARTISTS_DIR}${musicArray[i].artistImg}`);
-    musicImg.forEach(img => img.src = `${ALBUMS_DIR}${musicArray[i].img}`);
-    ft.textContent = musicArray[i].ft;
-
-    //Add a <br /> tag at every line break
-    lyrics.innerHTML = musicArray[i].lyrics.split('\n').join('<br /><br />');
-
-    //The source/url of the current song
-    mainAudio.src = `${SONGS_DIR}${musicArray[i].source}`;
-
-    //Change the url directory according to the current song name
-    window.location.hash = musicArray[i].name.toLowerCase();
-
-    // save the current song name in local storage
-    localStorage.setItem('lastPlayedSongName', musicArray[i].name.toLowerCase());
-
-    var image = new Image();
-    image.src = `${ALBUMS_DIR}${musicArray[i].img}`;
-    image.addEventListener("load", function () {
-        var colorThief = new ColorThief();
-        var dominantColor = colorThief.getColor(image);
-        document.body.style.backgroundColor = `rgb(${dominantColor})`;
-
-        if (dominantColor[0] < 40 && dominantColor[1] < 40 && dominantColor[2] < 40) {
-            header.style.backgroundColor = 'rgb(50 118 168 / 0.5)';
-            headerOverlay.style.backgroundColor = 'rgb(0 0 0 / 0.45)';
-        }
-        else {
-            header.style.backgroundColor = 'rgb(0 0 0 / 0.45)';
-        };
-
-        window.onscroll = () => {
-            if (window.scrollY > 390) {
-                header.style.backgroundColor = `rgb(${dominantColor})`;
-                if (dominantColor[0] < 40 && dominantColor[1] < 40 && dominantColor[2] < 40) header.style.backgroundColor = 'rgb(50 118 168)';
-                headerBtn.classList.add('active');
-                headerOverlay.style.backgroundColor = 'rgb(0 0 0 / 0.45)';
-            } else if (window.scrollY < 390 && dominantColor[0] < 40 && dominantColor[1] < 40 && dominantColor[2] < 40) {
-                    header.style.backgroundColor = 'rgb(50 118 168 / 0.5)';
-                    headerBtn.classList.remove('active')
-            }
-            else {
-                header.style.backgroundColor = 'rgb(0 0 0 / 0.45)';
-                headerOverlay.style.backgroundColor = 'transparent';
-                headerBtn.classList.remove('active');
-            };
-        };
-    });
-};
-
-window.addEventListener('unload', () => {
-    const currentTime = mainAudio.currentTime;
-    localStorage.setItem('currentTime', currentTime);
-});
-
-window.addEventListener('load', () => {
-    // Load the current time of the song from local storage if available
-    const currentTime = localStorage.getItem('currentTime');
-    if (currentTime) {
-    mainAudio.currentTime = currentTime;
-    // updateTime({ target: mainAudio });
+    /**
+ * Adds event listeners to the DOM elements.
+ */
+bindEvents() {
+  window.addEventListener('load', () => this.onWindowLoad());
+  window.addEventListener('unload', () => this.onWindowUnload());
+  this.playPauseButton.forEach(button => button.onclick = () => this.togglePlayPause());
+  this.smallPlayButton.onclick = () => this.togglePlayPause();
+  this.prev.onclick = () => this.prevSong();
+  this.next.onclick = () => this.nextSong();
+  this.mainAudio.ontimeupdate = (e) => this.updateProgress(e);
+  this.progressArea.onclick = (e) => this.setProgress(e);
+  this.mainAudio.onended = () => this.nextSong();
 }
-});
 
-const playMusic = () => {
-    wrapper.classList.add('paused');
-    playPauseButton.forEach(button => button.innerHTML = '<i class="fa-solid fa-circle-pause"></i>');
-    smallPlayButton.innerHTML = '<i class="fa-solid fa-pause"></i>';
-    mainAudio.play();
-};
+    /**
+ * Loads and plays the music at the given index from the musicArray.
+ *
+ * @param {number} index - The index of the music to be loaded.
+ * @returns {void}
+ */
+loadMusic(index) {
+    // Check if the music at the given index exists
+    if (!this.musicArray[index]) {
+        console.error(`There's no song at index: ${index}`);
+        return;
+    }
 
-const pauseMusic = () => {
-    wrapper.classList.remove('paused');
-    playPauseButton.forEach(button => button.innerHTML = '<i class="fa-solid fa-circle-play"></i>');
-    smallPlayButton.innerHTML = '<i class="fa-solid fa-solid fa-play"></i>';
-    mainAudio.pause();
-};
+    // Get the music object at the given index
+    const song = this.musicArray[index];
 
-const prevSong = () => {
-    musicIndex--;
-    musicIndex < 0 ? musicIndex = musicArray.length - 1 : musicIndex = musicIndex;
-    loadMusic(musicIndex);
-    playMusic();
-};
+    // Update the document title with the song name and artist
+    document.title = `${song.name} - ${song.artist}`;
 
-const nextSong = () => {
-    musicIndex++;
-    musicIndex > musicArray.length - 1 ? musicIndex = 0 : musicIndex = musicIndex;
-    loadMusic(musicIndex);
-    playMusic();
-};
+    // Update the music name, artist, and image for each element
+    this.musicName.forEach(name => name.textContent = song.name);
+    this.artist.forEach(artist => artist.textContent = song.artist);
+    this.musicImg.forEach(img => img.src = `${this.albumsDir}${song.img}`);
 
-playPauseButton.forEach(button => {
-    button.onclick = () => {
-    const isMusicPlay = wrapper.classList.contains('paused');
-    isMusicPlay ? pauseMusic() : playMusic();
-};
+    // Update the featured artist text
+    this.feature.textContent = song.ft;
 
-});
+    // Update the lyrics text with the song lyrics, splitting by newline characters
+    this.lyrics.innerHTML = song.lyrics.split('\n').join('<br /><br />');
 
-smallPlayButton.onclick = () => {
-    const isMusicPlay = wrapper.classList.contains('paused');
-    isMusicPlay ? pauseMusic() : playMusic();
-};
+    // Update the main audio source with the song source
+    this.mainAudio.src = `${this.songsDir}${song.source}`;
 
-prev.onclick = () => {
-    prevSong();
-};
+    // Store the last played song name in local storage
+    localStorage.setItem('lastPlayedSongName', song.name.toLowerCase());
 
-next.onclick = () => {
-    nextSong();
-};
+    // Load the dominant color of the album image
+    this.loadDominantColor(`${this.albumsDir}${song.img}`);
+}
 
-mainAudio.ontimeupdate = e => {
-    const currentTime = e.target.currentTime;
-    const duration = e.target.duration;
+    /**
+ * Loads the dominant color of the album image using the ColorThief library.
+ *
+ * @param {string} imagePath - The path to the album image.
+ * @returns {void}
+ */
+loadDominantColor(imagePath) {
+    // Create a new Image object
+    let image = new Image();
 
+    // Set the source of the image
+    image.src = imagePath;
+
+    // Add an event listener for the 'load' event of the image
+    image.onload = () => {
+        // Create a new instance of ColorThief
+        let colorThief = new ColorThief();
+
+        // Get the dominant color of the image using ColorThief
+        let dominantColor = colorThief.getColor(image);
+
+        // Apply the dominant color to the music player
+        this.applyDominantColor(dominantColor);
+    };
+}
+
+    /**
+ * Applies the dominant color to the music player elements.
+ *
+ * @param {Array<number>} color - The RGB color values of the dominant color.
+ * @returns {void}
+ */
+applyDominantColor(color) {
+    // Set the background color of the body to the dominant color
+    document.body.style.backgroundColor = `rgb(${color})`;
+
+    // Calculate the lightness of the dominant color
+    let lightness = color.reduce((a, b) => a + b) / 3;
+
+    // Apply different background colors to the header based on the lightness
+    if (lightness < 40) {
+        this.header.style.backgroundColor = 'rgb(50, 118, 168, 0.5)';
+        this.headerOverlay.style.backgroundColor = 'rgba(0, 0, 0, 0.45)';
+    } else {
+        this.header.style.backgroundColor = 'rgba(0, 0, 0, 0.45)';
+    }
+
+    // Add an event listener for the 'scroll' event of the window
+    window.onscroll = () => {
+        // Change the header background color and add/remove 'active' class based on scroll position
+        if (window.scrollY > 390) {
+            this.header.style.backgroundColor = `rgb(${color})`;
+            this.headerBtn.classList.add('active');
+            this.headerOverlay.style.backgroundColor = 'rgba(0, 0, 0, 0.45)';
+        } else {
+            this.header.style.backgroundColor = lightness < 40? 'rgb(50, 118, 168, 0.5)' : 'rgba(0, 0, 0, 0.45)';
+            this.headerBtn.classList.remove('active');
+        }
+    };
+}
+
+    /**
+ * Event handler for the 'load' event of the window.
+ * This method retrieves the last played song's current time from local storage,
+ * sets the main audio's current time to the retrieved value,
+ * and updates the progress bar accordingly.
+ *
+ * @returns {void}
+ */
+onWindowLoad() {
+    // Retrieve the last played song's current time from local storage
+    const currentTime = parseFloat(localStorage.getItem('currentTime')) || 0;
+
+    // Set the main audio's current time to the retrieved value
+    this.mainAudio.currentTime = currentTime;
+
+    // Update the progress bar with the current time
+    this.updateProgress({ target: this.mainAudio });
+}
+
+    /**
+ * Event handler for the 'unload' event of the window.
+ * This method stores the current time of the main audio in local storage.
+ * When the user revisits the page, the stored time will be retrieved and used to set the audio's current time.
+ *
+ * @returns {void}
+ */
+onWindowUnload() {
+    // Store the current time of the main audio in local storage
+    localStorage.setItem('currentTime', this.mainAudio.currentTime);
+}
+
+    /**
+ * Toggles the play/pause state of the music player.
+ * If the music player is currently paused, it will play the music.
+ * If the music player is currently playing, it will pause the music.
+ *
+ * @returns {void}
+ */
+togglePlayPause() {
+    if (this.wrapper.classList.contains('paused')) {
+        this.pauseMusic();
+    } else {
+        this.playMusic();
+    }
+}
+
+    /**
+ * Plays the music in the music player.
+ * Adds 'paused' class to the wrapper, changes the play/pause button icons to pause,
+ * and plays the main audio.
+ *
+ * @returns {void}
+ */
+playMusic() {
+    this.wrapper.classList.add('paused');
+    this.playPauseButton.forEach(button => button.innerHTML = '<i class="fa-solid fa-circle-pause"></i>');
+    this.smallPlayButton.innerHTML = '<i class="fa-solid fa-pause"></i>';
+    this.mainAudio.play();
+}
+
+    /**
+ * Pauses the music in the music player.
+ * Removes 'paused' class from the wrapper, changes the play/pause button icons to play,
+ * and pauses the main audio.
+ *
+ * @returns {void}
+ */
+pauseMusic() {
+    this.wrapper.classList.remove('paused');
+
+    // Update the play/pause button icons to play for each button
+    this.playPauseButton.forEach(button => button.innerHTML = '<i class="fa-solid fa-circle-play"></i>');
+
+    // Update the small play/pause button icon to play
+    this.smallPlayButton.innerHTML = '<i class="fa-solid fa-play"></i>';
+
+    // Pause the main audio
+    this.mainAudio.pause();
+}
+
+    /**
+ * Plays the previous song in the music player.
+ * Updates the musicIndex to the previous song in the musicArray,
+ * loads the music at the new index, and plays the music.
+ *
+ * @returns {void}
+ */
+prevSong() {
+    // Calculate the previous music index, wrapping around if necessary
+    this.musicIndex = (this.musicIndex - 1 + this.musicArray.length) % this.musicArray.length;
+
+    // Load the music at the new index
+    this.loadMusic(this.musicIndex);
+
+    // Play the music
+    this.playMusic();
+}
+
+    /**
+ * Plays the next song in the music player.
+ * Updates the musicIndex to the next song in the musicArray,
+ * loads the music at the new index, and plays the music.
+ *
+ * @returns {void}
+ */
+nextSong() {
+    // Calculate the next music index, wrapping around if necessary
+    this.musicIndex = (this.musicIndex + 1) % this.musicArray.length;
+
+    // Load the music at the new index
+    this.loadMusic(this.musicIndex);
+
+    // Play the music
+    this.playMusic();
+}
+
+    /**
+ * Updates the progress bar and current time of the music player.
+ *
+ * @param {Event} e - The event object that triggered the function.
+ * @returns {void}
+ */
+updateProgress(e) {
+    // Extract the current time and duration from the event target
+    const { currentTime, duration } = e.target;
+
+    // Calculate the progress width based on the current time and duration
     let progressWidth = (currentTime / duration) * 100;
 
-    progressBar.style.width = progressWidth + '%';
+    // Update the width of the progress bar
+    this.progressBar.style.width = `${progressWidth}%`;
 
-    let musicCurrentTime = document.querySelector('.current-time'),
-        songDuration = document.querySelector('.max-duration'),
-        dura = document.querySelector('.duration');
-
-    mainAudio.addEventListener('loadeddata', () => {
-        let mainAdDuration = mainAudio.duration;
-        let totalMinutes = Math.floor(mainAdDuration / 60);
-        let totalSeconds = Math.floor(mainAdDuration % 60);
-
-        if (totalSeconds < 10) {
-            totalSeconds = `0${totalSeconds}`;
-        };
-
-        songDuration.textContent = `${totalMinutes}:${totalSeconds}`;
-        dura.textContent = `${totalMinutes}:${totalSeconds}`;
-
-    });
+    // Calculate the current time in minutes and seconds
     let currentMinutes = Math.floor(currentTime / 60);
     let currentSeconds = Math.floor(currentTime % 60);
 
-    if (currentSeconds < 10) {
-        currentSeconds = `0${currentSeconds}`;
-    };
+    // Format the current seconds to have a leading zero if necessary
+    if (currentSeconds < 10) currentSeconds = `0${currentSeconds}`;
+
+    // Update the current time text in the document
+    let musicCurrentTime = document.querySelector('.current-time');
     musicCurrentTime.textContent = `${currentMinutes}:${currentSeconds}`;
-};
+}
 
-progressArea.onclick = e => {
-    let progressWidth = progressArea.clientWidth;
+    /**
+ * Sets the progress of the music player based on the user's click on the progress area.
+ *
+ * @param {MouseEvent} e - The mouse event that triggered the function.
+ * @returns {void}
+ */
+setProgress(e) {
+    // Calculate the width of the progress area
+    const progressWidth = this.progressArea.clientWidth;
 
-    let clikedOffsetX = e.offsetX;
-    let songDuration = mainAudio.duration;
+    // Get the horizontal offset of the mouse click relative to the progress area
+    const clickedOffsetX = e.offsetX;
 
-    mainAudio.currentTime = (clikedOffsetX / progressWidth) * songDuration;
-    playMusic();
+    // Get the duration of the current song
+    const songDuration = this.mainAudio.duration;
 
-};
+    // Calculate the new current time based on the clicked position and the song duration
+    this.mainAudio.currentTime = (clickedOffsetX / progressWidth) * songDuration;
 
-mainAudio.onended = () => {
-    nextSong();
-};
+    // Play the music after setting the new progress
+    this.playMusic();
+}
+}
+// Initialize the MusicPlayer instance when the DOM content is loaded
+document.addEventListener('DOMContentLoaded', () => {
+    const player = new MusicPlayer();
+});
